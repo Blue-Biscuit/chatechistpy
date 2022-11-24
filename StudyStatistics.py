bc9f56d1837cb25f5a7e88f9992a174e1f87a5e5
Andrew Huffman <ahuffman9000@gmail.com>
2022-11-24 10:19:11 -0500

    Due date methods on StudyStatistics

--- StudyStatistics.py
+++ StudyStatistics.py
@@ -22,5 +22,27 @@ class StudyStatistics:
         self.studyCount = 0
         """The number of times this passage has been studied."""
 
+        self.correctInARow = 0
+        """The number of times this passage has been reproduced correctly in a row."""
+
         self.dueDate = datetime.date.today()
         '''This passage\'s "due date," or the next time it should be studied.'''
+    
+    def isDue(self) -> bool:
+        """True if the passage is due today."""
+        return self.dueDate == datetime.date.today()
+    
+    def updateDueDate(self, correct: bool):
+        """Updates the due date after the passage has been studied."""
+        if correct:
+            self.correctInARow += 1
+        else:
+            self.correctInARow = 0
+        
+        self.dueDate = self._getNextDueDate()
+    
+    def _getNextDueDate(self) -> datetime.date:
+        """Calculates the next due date, based on the number of correct
+        reproductions in a row. Assumes this value has already been updated."""
+
+        return datetime.timedelta(days=int(self.correctInARow * 1.5)) + datetime.date.today()
