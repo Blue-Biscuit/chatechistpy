ef46ec4cfd3e68430fbdac46479f5b6ecbe0dfc4
Blue-Biscuit <ahuffman9000@gmail.com>
2022-11-24 11:38:23 -0500

    Version 1.1: The Study Update (#11)
    * learn command. (#1)
    
    * Learn command and method set up.
    
    * Added type flag to compare method on Passage
    
    * Error print if not enough args given to the learn command.
    
    * getPassage console UI helper method.
    
    * Fixes to the getPassage method, to mirror the more complex usage in the print command.
    
    * getPassage now used in the printCommand method.
    
    * Passage class iterable.
    
    * Loop structure of learn command.
    
    * Blanked printing loop.
    
    * Working game loop
    
    * console clearing and halting between turns.
    
    * getWord method.
    
    * mistake prints on learn command. (#2)
    
    * Learn command and method set up.
    
    * Added type flag to compare method on Passage
    
    * Error print if not enough args given to the learn command.
    
    * getPassage console UI helper method.
    
    * Fixes to the getPassage method, to mirror the more complex usage in the print command.
    
    * getPassage now used in the printCommand method.
    
    * Passage class iterable.
    
    * Loop structure of learn command.
    
    * Blanked printing loop.
    
    * Working game loop
    
    * console clearing and halting between turns.
    
    * getWord method.
    
    * Mistake prints on incorrect passage input
    
    * save and load commands (#3)
    
    * JSON serialization for Passage
    
    * save command.
    
    * load command
    
    * "rote" command (#5)
    
    * Rote memorization command implemented, no function yet
    
    * Initial implementation
    
    * Fixed printing issues
    
    * StudyStatistics class (#6)
    
    * __init__ magic method, with timing fields.
    
    * Due date methods on StudyStatistics
    
    * Made statistic fields on StudyStatistics __init__ params, for serialization
    
    * StudyStatistics serialization (#7)
    
    * toJSON method
    
    * serialization methods
    
    * 'statistics' field on Passage class (#8)
    
    * init method
    
    * Serialization
    
    * Serialization fix as it regards date objects
    
    * 'due' command (#9)
    
    * Initial implementation
    
    * More helpful print on command
    
    * 'study' command. (#10)
    
    * signals on the rote command to tell how the user exited
    
    * Initial implementation
    
    * Study command registered
    
    * Fixed list syntax
    
    * Properly skip passages which are not due for study.
    
    * Removed parameter from StudyStatistics.updateDueDate (unnecessary)
    
    * Slight change to the "due" command's printing format.

---
+++ StudyStatistics.py
@@ -0,0 +1,68 @@
+################################################################################
+#   StudyStatistics.py
+#
+#   Description:
+#       A data class which holds statistics about a study passage.
+#   Author:
+#       Andrew Huffman
+################################################################################
+
+import datetime
+import json
+
+class StudyStatistics:
+    """A data class which holds statistics about a study passage."""
+
+    def __init__(self, passageID: int, lastStudied = datetime.date.min, studyCount = 0, correctInARow = 0, dueDate = datetime.date.today()):
+        self.passageID = passageID
+        """The ID of the passage which this object tracks."""
+        
+        self.lastStudied = lastStudied
+        """The last time this passage was studied."""
+
+        self.studyCount = studyCount
+        """The number of times this passage has been studied."""
+
+        self.correctInARow = correctInARow
+        """The number of times this passage has been reproduced correctly in a row."""
+
+        self.dueDate = dueDate
+        '''This passage\'s "due date," or the next time it should be studied.'''
+    
+    def isDue(self) -> bool:
+        """True if the passage is due today."""
+        return self.dueDate == datetime.date.today()
+    
+    def updateDueDate(self):
+        """Updates the due date after the passage has been studied."""
+        self.dueDate = self._getNextDueDate()
+    
+    def _getNextDueDate(self) -> datetime.date:
+        """Calculates the next due date, based on the number of correct
+        reproductions in a row. Assumes this value has already been updated."""
+
+        return datetime.timedelta(days=int(self.correctInARow * 1.5)) + datetime.date.today()
+    
+    def toJSON(self) -> str:
+        """Creates a JSON string from the StudyStatistics instance."""
+
+        def json_default(x):
+            if isinstance(x, datetime.date):
+                return {'day':x.day, 'month':x.month, 'year':x.year}
+            else:
+                return x.__dict__
+
+        return json.dumps(self, default=lambda o: json_default(o))
+    
+    def fromDict(d: dict):
+        """Builds a StudyStatistics instance from a dictionary."""
+
+        def dateFromDict(d):
+            return datetime.date(d['year'], d['month'], d['day'])
+
+        return StudyStatistics(d["passageID"], dateFromDict(d["lastStudied"]), d["studyCount"], d["correctInARow"], dateFromDict(d["dueDate"]))
+    
+    def fromJSON(s: str):
+        """Builds a StudyStatistics instance from a json string."""
+        d = json.loads(s)
+        return StudyStatistics.fromDict(d)
--- consoleui.py
+++ consoleui.py
@@ -2,6 +2,7 @@ import Passage
 import helpers
 import random
 import time
+import datetime
 
 def getPassage(passages: list[Passage.Passage], args: list[str], selectionArgLoc: int) -> Passage.Passage | None:
     """Gets a passage based on id (if the string is int-parsable) or name."""
@@ -110,6 +111,10 @@ def helpCommand(args: list[str], passages: list[Passage.Passage]):
         print(f"{x}\t\t{COMMANDS[x]['help']}")
     print()
 
+LEARN_EXIT_SIGNAL = 0
+LEARN_OK_SIGNAL = 1
+LEARN_ERROR_SIGNAL = 2
+
 def learnCommand(args: list[str], passages: list[Passage.Passage]):
     """Plays a game with the selected passage."""
 
@@ -127,13 +132,13 @@ def learnCommand(args: list[str], passages: list[Passage.Passage]):
     if len(args) == 1:
         print("usage: learn <title | id>")
         print()
-        return
+        return LEARN_ERROR_SIGNAL
 
     p = getPassage(passages, args, 1)
     if p == None:
         print(f'Passage "{helpers.joinAfter(args, 1)}" not found')
         print()
-        return
+        return LEARN_ERROR_SIGNAL
 
     helpers.clearConsole()
 
@@ -190,8 +195,8 @@ def learnCommand(args: list[str], passages: list[Passage.Passage]):
 
         i = input(">>> ")
 
-        if i == 'exit':
-            break
+        if i.lower() == 'exit':
+            return LEARN_EXIT_SIGNAL
 
         compareResult = p.compare(i)
 
@@ -236,6 +241,81 @@ def learnCommand(args: list[str], passages: list[Passage.Passage]):
 
             time.sleep(SECS_BETWEEN_TURNS)
             helpers.clearConsole()
+
+    return LEARN_OK_SIGNAL
+
+ROTE_EXIT_SIGNAL = 0
+ROTE_CORRECT_SIGNAL = 1
+ROTE_INCORRECT_SIGNAL = 2
+ROTE_ERROR_SIGNAL = 3
+
+def roteCommand(args: list[str], passages: list[Passage.Passage]):
+    """Simple command to study a passage by rote typing it. Returns an exit code."""
+
+    # ALGORITHM:
+    # 1. Input parse. If input wasn't sufficient, error and exit.
+    # 2. Ask the user to reproduce the passage.
+    # 3. Notify the user whether they correctly reproduced the passage or not.
+
+    # 1. Input parse. If input wasn't sufficient, error and exit.
+
+    # If no args are given, print usage data.
+    if len(args) == 1:
+        print("usage: learn <title | id>")
+        print()
+        return ROTE_ERROR_SIGNAL
+
+    p = getPassage(passages, args, 1)
+    if p == None:
+        print(f'Passage "{helpers.joinAfter(args, 1)}" not found')
+        print()
+        return ROTE_ERROR_SIGNAL
+
+    helpers.clearConsole()
+
+    # 2. Ask the user to reproduce the passage.
+
+    print(f'Reproduce "{p.title}"')
+    print()
+
+    i = ''
+    while i == '':
+        i = input(">>> ").strip()
+
+        # If 'exit' was input, exit.
+        if i.lower() == 'exit':
+            print()
+            return ROTE_EXIT_SIGNAL
+
+    # 3. Notify the user whether they correctly reproduced the passage or not.
+
+    matchResult = p.compare(i)
+
+    result = ROTE_ERROR_SIGNAL
+    print()
+    if matchResult[0] == Passage.Passage.MATCH and matchResult[1] == Passage.Passage.MATCH:
+        print("Congratulations! That was correct.")
+        result = ROTE_CORRECT_SIGNAL
+    else:
+        print("Sorry, that was incorrect.")
+        print()
+
+        result = ROTE_INCORRECT_SIGNAL
+
+        if matchResult[0] == Passage.Passage.INPUT_TOO_SHORT:
+            print("The input was too short.")
+        elif matchResult[0] == Passage.Passage.INPUT_TOO_LONG:
+            print("The input was too long.")
+        else:
+            pI = Passage.Passage("", i, -1, [])
+            if matchResult[1] == Passage.Passage.INPUT_TOO_SHORT:
+                print(f'Input word {matchResult[0]}, "{pI.getWord(matchResult[0])}", was too short (should have been "{p.getWord(matchResult[0])}")')
+            elif matchResult[1] == Passage.Passage.INPUT_TOO_LONG:
+                print(f'Input word {matchResult[0]}, "{pI.getWord(matchResult[0])}", was too long (should have been "{p.getWord(matchResult[0])}")')
+            else:
+                print(f'Input word {matchResult[0]}, "{pI.getWord(matchResult[0])}", character {matchResult[1]}, was incorrect (should have been "{p.getWord(matchResult[0])}")')
+    print()
+    return result
 
 def saveCommand(args: list[str], passages: list[Passage.Passage]):
     """Saves the current passages list to a file."""
@@ -308,6 +388,103 @@ def loadCommand(args: list[str], passages: list[Passage.Passage]):
     for x in l:
         passages.append(x)
 
+def dueCommand(args: list[str], passages: list[Passage.Passage]):
+    """Prints the due date of the selected passage."""
+
+    # If no args are given, print usage data.
+    if len(args) == 1:
+        print("usage: due <title | id>")
+        print()
+        return
+
+    p = getPassage(passages, args, 1)
+    if p == None:
+        print(f'Passage "{helpers.joinAfter(args, 1)}" not found')
+        print()
+        return
+    
+    fromNow = p.statistics.dueDate - datetime.date.today()
+    parenthetical = ''
+
+    if fromNow.days == 0:
+        parenthetical = 'today'
+    elif fromNow.days < 0:
+        parenthetical = f'{fromNow.days * -1} day{"s" if fromNow.days < -1 else ""} ago'
+    else:
+        parenthetical = f'{fromNow.days} day{"s" if fromNow.days > 1 else ""} from now'
+    print(f'{p.statistics.dueDate} ({parenthetical})')
+
+    print()
+
+def studyCommand(args: list[str], passages: list[Passage.Passage]):
+    """Studies all due passages, and updates their statistics accordingly."""
+
+    # ALGORITHM:
+    # 1. Loop through the passages list, studying those that are due.
+
+    # 1. Loop through the passages list, studying those that are due.
+    studiedOne = False
+
+    for p in passages:
+        # ALGORITHM:
+        # 1. If a passage has been studied before, "learn" it.
+        # 2. Otherwise, "rote" it.
+        # 3. Update statistics.
+
+        if not p.statistics.isDue():
+            continue
+        
+        studiedOne = True
+
+        correctInARow = p.statistics.correctInARow
+
+        # 1. If a passage has been studied before, "learn" it.
+
+        if p.statistics.studyCount == 0:
+            cmd = ["learn", p.title]
+            sg = learnCommand(cmd, passages)
+
+            # If the user sent an exit signal, exit the study command.
+            if sg == LEARN_EXIT_SIGNAL:
+                return
+            else:
+                correctInARow += 1
+
+        # 2. Otherwise, "rote" it.
+
+        else:
+            cmd = ["rote", p.title]
+            sg = roteCommand(cmd, passages)
+
+            # If the user sent an exit signal, exit the study command.
+            if sg == ROTE_EXIT_SIGNAL:
+                return
+            
+            # If the user was incorrect, relearn.
+            if sg == ROTE_INCORRECT_SIGNAL:
+                correctInARow = 0
+                cmd2 = ["learn", p.id]
+                sg = learnCommand(cmd2, passages)
+
+                # If the user sent an exit signal, exit the study command.
+                if sg == LEARN_EXIT_SIGNAL:
+                    return
+                else:
+                    correctInARow += 1
+
+        # 3. Update statistics.
+
+        p.statistics.correctInARow = correctInARow
+        p.statistics.studyCount += 1
+        p.statistics.updateDueDate()
+
+    if studiedOne:
+        print("Study session complete.")
+        print()
+    else:
+        print("Nothing to study right now.")
+        print()
+
 COMMANDS = {
     "new" : {
         "help" : "creates a new passage.",
@@ -321,9 +498,21 @@ COMMANDS = {
         "help" : "lists passages.",
         "method" : listCommand
     },
+    "study" : {
+        "help" : "studies all new commands while updating study statistics.",
+        "method" : studyCommand
+    },
     "learn" : {
         "help" : "plays a memorization game with the provided passage.",
         "method" : learnCommand
+    },
+    "rote" : {
+        "help" : "tests a provided passage by asking for its content without hints.",
+        "method" : roteCommand
+    },
+    "due" : {
+        "help" : "prints the due date of a passage.",
+        "method" : dueCommand
     },
     "save" : {
         "help" : "saves the current passages list to a file.",
